5f406c397a098264160ec1b1a04ff862c26cd508
Mara <mara-li@outlook.fr>
2021-10-16 08:46:59 +0200

    :memo: typo

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Natural Language :: English",
         "Natural Language :: French",
-        "Topic :: Text Processing :: Markup :: Markdown"
+        "Topic :: Text Processing :: Markup :: Markdown",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
